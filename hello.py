f7a883eb2846ddcc0fa129cb71d799366527309c
yozamacs <yozamacs@yahoo.com>
2014-10-16 15:03:09 -0700

    debugging heroku

--- hello.py
+++ hello.py
@@ -11,9 +11,6 @@ def test():
 @app_lulu.route('/home', methods=['GET','POST'])
 def home():
 	schoolList = []
-    # this is a comment, just like in Python
-    # note that the function name and the route argument
-    # do not need to be the same.
 	if request.method == "GET":
 	    	for f in os.listdir("CSVs"):
 			if f.endswith(".csv"):
@@ -23,16 +20,14 @@ def home():
    		app_lulu.checkboxValues = request.form
 		if len(app_lulu.checkboxValues)==0:
 			outcome = 'no checkboxes checked'
-			return render_template('submit.html', outcome=outcome)
 		else:
    			for key in app_lulu.checkboxValues:
    				schoolName = key.strip('\'')
 				if(subprocess.call(['java', '-jar', 'tw-recruiting.jar', 'CSVs/'+schoolName])!=0):
 					outcome = 'failure'
 					#output = subprocess.check_output(['ls', '-1']) wanted to get error log....do that in the future
-					return render_template('submit.html', outcome=outcome)
-			outcome = 'success'
-			return render_template('submit.html', outcome=outcome)
+				outcome = 'success'
+		return render_template('submit.html', outcome=outcome)
 
 
 if __name__ == '__main__':
